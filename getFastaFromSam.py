c83af8cf5e87ff28ea4205eecd3d25d9de0a7f52
neavemj <matthewjneave1@gmail.com>
2015-06-02 11:11:19 +0300

    added argparse and sam file parsing

--- getFastaFromSam.py
+++ getFastaFromSam.py
@@ -5,5 +5,41 @@
 
 import argparse
 
+# get command line arguments with argparse
+
+parser = argparse.ArgumentParser("gets reads from sam file that map to particular contigs")
+
+parser.add_argument("sam_file", type = argparse.FileType("r"), nargs = 1,
+        help = "sam file to extract reads from")
+parser.add_argument("contig_list", type = argparse.FileType("r"), nargs = 1,
+        help = "extract reads that match to these contigs")
+parser.add_argument("output_name", type = argparse.FileType("w"), nargs = 1,
+        help = "fasta file output name")
+
+args = parser.parse_args()
+
+
+# create list of target contigs
+
+target_contigs = []
+
+for contig in args.contig_list:
+    contig = contig.strip()
+    target_contigs.append(contig)
+
+# now extract reads that map to these contigs and write to fasta
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
