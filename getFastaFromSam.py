856617f0eb537ad02ee8efc03b8f2196ec2d6311
neavemj <matthewjneave1@gmail.com>
2015-06-02 11:48:54 +0300

    fasta file writing

--- getFastaFromSam.py
+++ getFastaFromSam.py
@@ -4,6 +4,13 @@
 # Matthew J. Neave 2.6.15
 
 import argparse
+import re 
+
+"""
+uses regex to match header names so need to be careful with contig list
+eg. NODE_1 also matches NODE_100, put following character to avoid
+NODE_1_ no longer matches higher up numbers
+"""
 
 # get command line arguments with argparse
 
@@ -13,7 +20,7 @@ parser.add_argument("sam_file", type = argparse.FileType("r"), nargs = 1,
         help = "sam file to extract reads from")
 parser.add_argument("contig_list", type = argparse.FileType("r"), nargs = 1,
         help = "extract reads that match to these contigs")
-parser.add_argument("output_name", type = argparse.FileType("w"), nargs = 1,
+parser.add_argument("output", type = argparse.FileType("w"), nargs = 1,
         help = "fasta file output name")
 
 args = parser.parse_args()
@@ -23,23 +30,16 @@ args = parser.parse_args()
 
 target_contigs = []
 
-for contig in args.contig_list:
+for contig in args.contig_list[0]:
     contig = contig.strip()
     target_contigs.append(contig)
 
 # now extract reads that map to these contigs and write to fasta
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for record in args.sam_file[0]:
+    record = record.strip()
+    cols = record.split("\t")
+    if not record.startswith("@"):
+        for contig in target_contigs:
+            if re.search(contig, cols[2]): # see note about regex
+                args.output[0].write(">%s\n%s\n" % (cols[0], cols[9]))
