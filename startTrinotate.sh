1d94768c0fece3ebf4a9d84e6f5e72b8b9045299
neavemj <matthewjneave1@gmail.com>
2015-12-10 08:41:13 +1100

    minor formatting

--- startTrinotate.sh
+++ startTrinotate.sh
@@ -1,4 +1,6 @@
 cd ~/software/Trinotate-2.0.2/TrinotateWeb
 module load perl
+echo "browser site: http://bragg-gpu.hpc.csiro.au:8080/cgi-bin/index.cgi"
+echo "data: /data/nea040/1.prawnRNAi/7.sqliteResults/Trinotate.sqlite"
 ./matt_run_mongoose_webserver.sh
 
