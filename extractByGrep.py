2750f775398e4a6a46d1da3cdb5906985c3baf41
neavemj <matthewjneave1@gmail.com>
2016-04-26 11:29:25 +1000

    extract contigs from fasta file using grep matches in header line

---
+++ extractByGrep.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Matthew J Neave 26.04.2016 
+# extract sequences using a grep pattern 
+# input fasta first, grep pattern, then a name for the output
+
+import sys
+from Bio import SeqIO
+import re
+
+fasta_file = sys.argv[1]  # input fasta file
+grep_pat = sys.argv[2] # grep pattern
+result_file = sys.argv[3] # output name
+
+pat = re.compile(grep_pat)
+
+count = 0
+fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
+end = False
+with open(result_file, "w") as f:
+    for seq in fasta_sequences:
+        if re.findall(pat, seq.id):
+            SeqIO.write([seq], f, "fasta")
+            count += 1       # keep track of how many IDs were found so can report later
+
+print "Saved %i records from %s to %s" % (count, fasta_file, result_file)
+
