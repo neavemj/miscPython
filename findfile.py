f0d189d8a4fc027dfcaad8861b1ce59c9843ff1e
neavemj <matthewjneave1@gmail.com>
2016-09-14 12:15:58 +1000

    updated to python 3

--- findfile.py
+++ findfile.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# python 3
 
 # quick find file script
 
@@ -10,4 +11,4 @@ for path, dirs, files in os.walk("."):
     for fl in files:
         if re.findall(name, fl):
             full_path = os.path.join(path, fl)
-            print os.path.abspath(full_path)
+            print(os.path.abspath(full_path))
