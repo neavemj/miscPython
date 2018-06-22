58d3b9aab4b2b654b391cf81b8ed4dd58eafebcd
neavemj <matthewjneave1@gmail.com>
2018-06-22 14:06:06 +1000

    added pandas plotting and argparse

--- kmer_peak_drawer.py
+++ kmer_peak_drawer.py
@@ -5,7 +5,7 @@
 
 import argparse
 import subprocess # use to pass command line calls
-import os
+import os, sys
 import pandas as pd
 import matplotlib
 matplotlib.use("Agg") # avoids 'no display' error
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 # argparse to collect command line arguments
 
-parser = argparse.ArgumentParser("take raw NGS reads, trim, calculate 31-mer peaks, and produce a graph\nnote: requires trimmomatic and bbmap to be present on the path\n")
+parser = argparse.ArgumentParser("take raw NGS reads, trim, calculate 31-mer peaks, and produce a graph\nnote: requires trimmomatic and bbmap to be present on the path\nnote: requires pandas and matplotlib to be installed in python\nnote: the bbmap step can use a lot of memory (around 70 Gb for a HiSeq lane)\nnote: the stem of input files should be separated by an underscore\n")
 
 parser.add_argument('-1', '--forward_reads', type = str,
         nargs=1, help = "raw fastq forward reads")
@@ -21,6 +21,10 @@ parser.add_argument('-2', '--reverse_reads', type = str,
         nargs=1, help = "raw fastq reverse reads")
 parser.add_argument('-t', '--threads', type = str,
         nargs="?", default="16", help = "threads for trimming and khist [default 16]")
+
+if len(sys.argv) == 1:  # if no args are given
+    parser.print_help(sys.stderr)
+    sys.exit(1)
 
 args = parser.parse_args()
 
@@ -34,14 +38,30 @@ except:
     print("\n~~~ could not find adapter file for trimmomatic! ~~~\n")
     raise
 
+# check that modules have been loaded
+
+try:
+    subprocess.call(["trimmomatic", "-version"])
+except OSError as e:
+    if e.errno == os.errno.ENOENT:
+        print("\ntrimmomatic could not be found: try 'module load trimmomatic'\n")
+        raise
+
+try:
+    tmp = subprocess.call(["khist.sh", "-version"])
+except OSError as e:
+    if e.errno == os.errno.ENOENT:
+        print("\nkhist could not be found: try 'module load bbmap'\n")
+        raise
+
 # grab stem name of files for later
 
 stem = args.forward_reads[0].split("_")[0]
 
-# the reads first need to be trimmed
+## TRIM READS ##
 print("~~~ beginning trimming with trimmomatic ~~~")
 
-subprocess.call(["trimmomatic", "PE", "-threads", args.threads, args.forward_reads[0], args.reverse_reads[0], "-baseout", stem+".fastq.gz", "ILLUMINACLIP:" + adapter_path + ":2:30:10", "LEADING:3", "TRAILING:3", "SLIDINGWINDOW:4:20", "MINLEN:50"])
+subprocess.check_output(["trimmomatic", "PE", "-threads", args.threads, args.forward_reads[0], args.reverse_reads[0], "-baseout", stem+".fastq.gz", "ILLUMINACLIP:" + adapter_path + ":2:30:10", "LEADING:3", "TRAILING:3", "SLIDINGWINDOW:4:20", "MINLEN:50"])
 
 forward_trimmed = args.forward_reads[0].rstrip("R1.fastq.gz") + "1P.fastq.gz"
 reverse_trimmed = args.reverse_reads[0].rstrip("R2.fastq.gz") + "2P.fastq.gz"
