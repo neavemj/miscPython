fbd53006af32cffa994466b1c07139d851abe3ed
neavemj <matthewjneave1@gmail.com>
2020-05-22 12:10:23 +1000

    small updates

--- extractByGrep.py
+++ extractByGrep.py
@@ -9,19 +9,23 @@ from Bio import SeqIO
 import re
 
 fasta_file = sys.argv[1]  # input fasta file
-grep_pat = sys.argv[2] # grep pattern
+grep_pat = open(sys.argv[2]).readlines() # grep pattern
 result_file = sys.argv[3] # output name
 
-pat = re.compile(grep_pat)
+re_pat = [re.compile(pat.strip()) for pat in grep_pat]
+
+#pat = re.compile(grep_pat)
 
 count = 0
 fasta_sequences = SeqIO.parse(open(fasta_file),'fasta')
 end = False
 with open(result_file, "w") as f:
     for seq in fasta_sequences:
-        if re.findall(pat, seq.id):
-            SeqIO.write([seq], f, "fasta")
-            count += 1       # keep track of how many IDs were found so can report later
+        for pat in re_pat:
+            if re.findall(pat, seq.id) or \
+               re.findall(pat, seq.description):
+                SeqIO.write([seq], f, "fasta")
+                count += 1       # keep track of how many IDs were found so can report later
 
 print("Saved {} records from {} to {}".format(count, fasta_file, result_file))
 
--- name_contig_by_filename.py
+++ name_contig_by_filename.py
@@ -5,24 +5,18 @@
 # which often have weird contig names
 # Matthew J. Neave 12.07.2018
 
-import sys
+import sys, os
 
 fasta = open(sys.argv[1])
-file_name = sys.argv[1].rstrip(".fasta")
+file_name = os.path.basename(sys.argv[1]).rstrip(".fasta")
 output = open(sys.argv[2], "w")
-
 
 contig_count = 0
 
 for line in fasta:
-    if contig_count > 1:
-        print(file_name)
-        print("error there is more than one contig in this file\n"
-              "doesn't make sense to rename this way")
-        sys.exit(1)
     if line.startswith(">"):
         contig_count += 1
-        output.write(">" + file_name + "\n")
+        output.write(">" + file_name + "_" + str(contig_count) + "\n")
     else:
         output.write(line)
 
